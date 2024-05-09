731e1388b7c0c16853e3ce8fdacaaab323767db1
TadhgCahill <tadhg.cahill@gmail.com>
2024-05-09 13:14:37 -0700

    Delete logon.js

--- logon.js
+++
@@ -1,5 +0,0 @@
-//checks if username already exists
-//checks that password == repeat password
-//check that password is at least 8 chars
-//check that password has at least one number
-//check that password has non alpha numeric character 
